ffc73c056808f568cfdad13b77f6f2fd8674f3cd
Maciej Wilczyński <maciej.wilczynski@letsgodevops.pl>
2022-11-29 17:32:16 +0100

    fix: set tls provider version constraint

--- versions.tf
+++ versions.tf
@@ -4,11 +4,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4"
+      version = ">= 4.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2"
+      version = ">= 2.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0"
     }
   }
 }
