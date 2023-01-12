9498acf1a94001e495158e214f4944d112533628
Maciej Wilczyński <maciej.wilczynski@letsgodevops.pl>
2023-01-12 09:26:14 +0100

    fix: add missing region into kube-ops role name

--- iam-kube-ops.tf
+++ iam-kube-ops.tf
@@ -44,7 +44,7 @@ data "aws_iam_policy_document" "kube_ops" {
 }
 
 resource "aws_iam_role" "kube_ops" {
-  name               = "kube-ops-${var.cluster_name}"
+  name               = "kube-ops-${var.cluster_name}-${data.aws_region.current.name}"
   path               = "/system/"
   assume_role_policy = data.aws_iam_policy_document.kube_ops_assume_role_policy.json
 }
