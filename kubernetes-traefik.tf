b219fa6db3241009588104508c7cf2321ef869d1
Bartosz Jasinski <bartosz.jasinski@toolsforhumanity.com>
2023-08-04 11:21:41 +0200

    feat: create two lists for external and internal LBs

--- kubernetes-traefik.tf
+++ kubernetes-traefik.tf
@@ -3,6 +3,9 @@ locals {
     { "traefik" = true },
     { for key, value in var.custom_load_balancers : "traefik-${key}" => value },
   )
+
+  internal_load_balancers = [for k, v in local.load_balancers : k if v == false]
+  external_load_balancers = [for k, v in local.load_balancers : k if v == true]
 }
 
 resource "kubernetes_namespace" "traefik" {
