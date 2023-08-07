49620442133cf88634f882dfccf4e6a129a574b8
Bartosz Jasinski <bartosz@jasinski.e>
2023-08-07 10:48:09 +0200

    fix: change list to set

--- kubernetes-traefik.tf
+++ kubernetes-traefik.tf
@@ -4,8 +4,8 @@ locals {
     { for key, value in var.custom_load_balancers : "traefik-${key}" => value },
   )
 
-  internal_load_balancers = [for k, v in local.load_balancers : k if v == false]
-  external_load_balancers = [for k, v in local.load_balancers : k if v == true]
+  internal_load_balancers = toset([for k, v in local.load_balancers : k if v == false])
+  external_load_balancers = toset([for k, v in local.load_balancers : k if v == true])
 }
 
 resource "kubernetes_namespace" "traefik" {
