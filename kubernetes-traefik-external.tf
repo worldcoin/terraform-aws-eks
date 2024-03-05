402f805e3e49f76e4ae827db1ca9c8fc73e7c8f2
Daniel Fenert <daniel.fenert@worldcoin.org>
2024-03-05 08:46:09 +0100

    Update kubernetes-traefik-external.tf

--- kubernetes-traefik-external.tf
+++ kubernetes-traefik-external.tf
@@ -85,7 +85,7 @@ resource "kubernetes_ingress_v1" "treafik_ingress" {
 }
 
 module "alb" {
-  source   = "git@github.com:worldcoin/terraform-aws-alb.git?ref=INFRA-1884"
+  source   = "git@github.com:worldcoin/terraform-aws-alb.git?ref=v0.4.0"
   for_each = toset([local.external_alb_name])
 
   # because of lenght limitation of LB name we need to remove prefix treafik from internal NLB
