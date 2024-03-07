cb15d5a2a326b0bc87b53bb0a669a8a17fafeb5e
Daniel Fenert <daniel.fenert@worldcoin.org>
2024-03-07 08:35:46 +0100

    Add second toleration for migration

--- cluster-addons.tf
+++ cluster-addons.tf
@@ -56,6 +56,10 @@ resource "aws_eks_addon" "coredns" {
         key : "critical"
       },
       {
+        effect : "NoExecute",
+        key : "karpenter"
+      },
+      {
         key : "CriticalAddonsOnly",
         operator : "Exists"
       },
