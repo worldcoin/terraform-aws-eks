9337c2ea083764c7978801dd7bf8a65822f89198
Bartosz Jasinski <bartosz.jasinski@toolsforhumanity.com>
2024-07-13 11:37:07 +0200

    feat: update module version

--- kubernetes-traefik-external.tf
+++ kubernetes-traefik-external.tf
@@ -91,7 +91,7 @@ resource "kubernetes_ingress_v1" "treafik_ingress" {
 }
 
 module "alb" {
-  source   = "git@github.com:worldcoin/terraform-aws-alb.git?ref=v0.5.1"
+  source   = "git@github.com:worldcoin/terraform-aws-alb.git?ref=v0.6.0"
   for_each = toset([local.external_alb_name])
 
   # because of lenght limitation of LB name we need to remove prefix treafik from internal NLB
