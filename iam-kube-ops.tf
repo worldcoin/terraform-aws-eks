d517bb83cbef0e5f3da25135586ab34d9fb948d9
Michał Leszczyński <michal.leszczynski@toolsforhumanity.com>
2024-07-18 11:08:51 +0200

    INFRA-2776 Move kube-ops back to eks module

---
+++ iam-kube-ops.tf
@@ -0,0 +1,52 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
+
+resource "aws_iam_role" "kube_ops" {
+  name = "kube-ops-${var.cluster_name}-${data.aws_region.current.name}"
+
+  assume_role_policy   = data.aws_iam_policy_document.assume_role.json
+  path                 = "/system/"
+}
+
+data "aws_iam_policy_document" "kube_ops" {
+  statement {
+    sid    = "readSecretsManager"
+    effect = "Allow"
+
+    actions = [
+      "secretsmanager:GetResourcePolicy",
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:ListSecretVersionIds",
+      "secretsmanager:ListSecrets"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "kube_ops" {
+  name   = "kube-ops-${var.cluster_name}"
+  role   = aws_iam_role.kube_ops.id
+  policy = data.aws_iam_policy_document.kube_ops.json
+}
+
+resource "aws_eks_pod_identity_association" "this" {
+  cluster_name    = var.cluster_name
+  namespace       = "kube-ops"
+  service_account = "kube-ops"
+  role_arn        = aws_iam_role.kube_ops.arn
+}
