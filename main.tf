c1e0be608699b14b84ccb30707410ad7402936c9
Krzysztof Szarek <krzysztof.szarek@toolsforhumanity.com>
2025-02-13 11:57:42 +0100

    Enable bootstrap_cluster_creator_admin_permissions

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_eks_cluster" "this" {
 
   access_config {
     authentication_mode                         = var.authentication_mode
-    bootstrap_cluster_creator_admin_permissions = false
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
   timeouts {
