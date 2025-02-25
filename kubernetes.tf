0a4f6dcc2dc28515786ef46e03ecafb9bedbfb35
Tomasz Charewicz <tomasz.charewicz@toolsforhumanity.com>
2025-02-25 11:30:36 +0100

    bugfixes with k8s provider

--- kubernetes.tf
+++ kubernetes.tf
@@ -7,11 +7,11 @@ data "aws_eks_cluster" "this" {
 
 data "aws_eks_cluster_auth" "this" {
   for_each = toset(data.aws_eks_clusters.this.names)
-  name     = data.aws_eks_cluster.this[each.value].endpoint
+  name     = data.aws_eks_cluster.this[each.value].name
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster_auth.this[var.cluster_name].name
+  host                   = data.aws_eks_cluster.this[var.cluster_name].endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this[var.cluster_name].certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this[var.cluster_name].token
 }
