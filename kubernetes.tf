8593eff2fa59ad14697211ad1e96a97fbbb7c445
Tomasz Charewicz <tomasz.charewicz@toolsforhumanity.com>
2025-02-25 12:21:43 +0100

    update to case when claster need to be created

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,3 +1,4 @@
+# List all clusters in the AWS account, read data about them and authenticate to them in a case when cluster is allredy created.
 data "aws_eks_clusters" "this" {}
 
 data "aws_eks_cluster" "this" {
@@ -10,8 +11,24 @@ data "aws_eks_cluster_auth" "this" {
   name     = data.aws_eks_cluster.this[each.value].name
 }
 
+# Aythenticate to the cluster in a case when cluster is gonna be created.
+data "aws_eks_cluster_auth" "default" {
+  name     = aws_eks_cluster.this.name
+}
+
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this[var.cluster_name].endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this[var.cluster_name].certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this[var.cluster_name].token
+  host = coalesce(
+    try(data.aws_eks_cluster.this[var.cluster_name].endpoint, null),
+    aws_eks_cluster.this.endpoint
+  )
+  cluster_ca_certificate = base64decode(
+    coalesce(
+      try(data.aws_eks_cluster.this[var.cluster_name].certificate_authority[0].data, null),
+      aws_eks_cluster.this.certificate_authority[0].data
+    )
+  )
+  token = coalesce(
+    try(data.aws_eks_cluster_auth.this[var.cluster_name].token, null),
+    data.aws_eks_cluster_auth.default.token
+  )
 }
