4453cfaabcd79326f46042e54a3e3f29e27745ca
Daniel Fenert <daniel.fenert@worldcoin.org>
2025-04-30 15:03:16 -0700

    Fix Node not ready monitor (#192)
    Fix Node not ready monitor, make better name for k8s monitors

--- datadog.tf
+++ datadog.tf
@@ -1,10 +1,10 @@
 module "datadog_monitoring" {
   count = var.monitoring_enabled ? 1 : 0
 
-  source = "git@github.com:worldcoin/terraform-datadog-kubernetes?ref=v1.2.1"
+  source = "git@github.com:worldcoin/terraform-datadog-kubernetes?ref=v1.2.2"
 
   notification_channel = var.monitoring_notification_channel
-  service              = "Kubernetes"
+  service              = format("EKS %s", var.cluster_name)
   env                  = var.environment
   filter_str           = format("kube_cluster_name:%s", var.cluster_name)
   additional_tags = [
