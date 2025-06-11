86ee375d929bf6b2d32a53bc954797b3c3ebf809
Marcin Janas <marcin.janas@letsgodevops.pl>
2025-06-11 17:36:47 +0200

    feat(kubernetes): add storage class
    Signed-off-by: Marcin Janas <marcin.janas@letsgodevops.pl>

---
+++ kubernetes-storage-class.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_storage_class" "ebs_gp3" {
+  count = var.kubernetes_provider_enabled ? 1 : 0
+
+  metadata {
+    name = "ebs-gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+
+  storage_provisioner    = "ebs.csi.aws.com"
+  volume_binding_mode    = "WaitForFirstConsumer"
+  allow_volume_expansion = true
+
+  parameters = {
+    "encrypted" = "true"
+    "fsType"    = "ext4"
+    "type"      = "gp3"
+  }
+}
