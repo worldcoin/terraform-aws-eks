e68402d40c600d54e5538a9158ac580eef534053
Michał Leszczyński <michal.leszczynski@worldcoin.org>
2025-07-09 12:06:03 +0200

    Add missing resource instance key

--- datadog.tf
+++ datadog.tf
@@ -89,7 +89,7 @@ resource "datadog_synthetics_test" "cluster_monitoring" {
   options_list {
     tick_every           = 300
     monitor_priority     = 1
-    min_location_failed  = min(var.monitoring_reachability_fail_locations, length(data.datadog_synthetics_locations.locations.locations))
+    min_location_failed  = min(var.monitoring_reachability_fail_locations, length(one(data.datadog_synthetics_locations.locations).locations))
     min_failure_duration = var.monitoring_reachability_failure_duration
     retry {
       count    = 3
