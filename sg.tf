c1e6d7f475fa20fb1e5d45e5f053dbf88d550760
Michał Leszczyński <michal.leszczynski@worldcoin.org>
2025-07-09 13:14:01 +0200

    Update sg rules to accept optional parameters

--- sg.tf
+++ sg.tf
@@ -111,25 +111,39 @@ resource "aws_security_group_rule" "node_from_alb_ingress" {
 resource "aws_security_group_rule" "additional_rule" {
   for_each = { for v in var.additional_security_group_rules : v.sg_id => v }
 
-  security_group_id        = aws_security_group.node.id
-  source_security_group_id = each.value.sg_id
-  type                     = each.value.type
-  from_port                = each.value.from_port
-  to_port                  = each.value.to_port
-  protocol                 = each.value.protocol
-  description              = each.value.description
+  # Required
+  security_group_id = aws_security_group.node.id
+  protocol          = each.value.protocol
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  type              = each.value.type
+
+  # Optional
+  description              = lookup(each.value, "description", null)
+  cidr_blocks              = lookup(each.value, "cidr_blocks", null)
+  ipv6_cidr_blocks         = lookup(each.value, "ipv6_cidr_blocks", null)
+  prefix_list_ids          = lookup(each.value, "prefix_list_ids", [])
+  self                     = lookup(each.value, "self", null)
+  source_security_group_id = try(each.value.source_cluster_security_group, false) ? aws_security_group.cluster.id : lookup(each.value, "sg_id", null)
 }
 
 resource "aws_security_group_rule" "additional_cluster_security_group_rules" {
   for_each = { for v in var.additional_cluster_security_group_rules : v.sg_id => v }
 
-  security_group_id        = aws_security_group.cluster.id
-  source_security_group_id = each.value.sg_id
-  type                     = each.value.type
-  from_port                = each.value.from_port
-  to_port                  = each.value.to_port
-  protocol                 = each.value.protocol
-  description              = each.value.description
+  # Required
+  security_group_id = aws_security_group.cluster.id
+  protocol          = each.value.protocol
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  type              = each.value.type
+
+  # Optional
+  description              = lookup(each.value, "description", null)
+  cidr_blocks              = lookup(each.value, "cidr_blocks", null)
+  ipv6_cidr_blocks         = lookup(each.value, "ipv6_cidr_blocks", null)
+  prefix_list_ids          = lookup(each.value, "prefix_list_ids", [])
+  self                     = lookup(each.value, "self", null)
+  source_security_group_id = try(each.value.source_node_security_group, false) ? aws_security_group.node.id : lookup(each.value, "sg_id", null)
 }
 
 resource "aws_security_group_rule" "tfe_and_gha_cluster_ingress" {
