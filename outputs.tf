7c05b5e30fd11d841b110db3508307c76a9b16ab
Michał Leszczyński <michal.leszczynski@worldcoin.org>
2025-07-10 10:35:54 +0200

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -55,10 +55,10 @@ output "node_security_group_id" {
 
 output "cluster_certificate_authority_data" {
   description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = try(aws_eks_cluster.this[0].certificate_authority[0].data, null)
+  value       = try(aws_eks_cluster.this.certificate_authority[0].data, null)
 }
 
 output "cluster_endpoint" {
   description = "Endpoint for your Kubernetes API server"
-  value       = try(aws_eks_cluster.this[0].endpoint, null)
+  value       = try(aws_eks_cluster.this.endpoint, null)
 }
