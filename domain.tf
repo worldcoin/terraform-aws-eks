4307420729ce1d96b8757fcc0f0a1db6383d2e83
Tomasz Charewicz <tomasz.charewicz@toolsforhumanity.com>
2025-10-21 14:46:03 +0200

    dummy resource to ensure state cleanup

--- domain.tf
+++ domain.tf
@@ -9,3 +9,10 @@ removed {
     destroy = false
   }
 }
+
+# Dummy resource to ensure state is updated even if cloudflare_record resources weren't created
+resource "null_resource" "cloudflare_migration_marker" {
+  triggers = {
+    migration_timestamp = timestamp()
+  }
+}
