94b3824cab4b53c51e00dacc4affe03e8335cb5e
Tomasz Charewicz <tomasz.charewicz@toolsforhumanity.com>
2025-10-21 14:38:19 +0200

    update nlb module

--- kubernetes-traefik-internal.tf
+++ kubernetes-traefik-internal.tf
@@ -48,7 +48,7 @@ resource "kubernetes_service" "traefik_nlb" {
 }
 
 module "nlb" {
-  source   = "git@github.com:worldcoin/terraform-aws-nlb.git?ref=v0.1.3"
+  source   = "git@github.com:worldcoin/terraform-aws-nlb.git?ref=v1.1.1"
   for_each = local.internal_load_balancers
 
   # because of lenght limitation of LB name we need to remove prefix treafik from custom_load_balancers
