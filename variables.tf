bb88e174088f7decd5a4c736b505537d6b5fd8ee
Eryk Smiech <eryk.smiech@toolsforhumanity.com>
2025-11-04 15:23:18 +0100

    INFRA-5406 Restrict CIDR access to EKS

--- variables.tf
+++ variables.tf
@@ -34,15 +34,15 @@ variable "cluster_version" {
 variable "vpc_config" {
   description = "VPC configuration from aws/vps module"
   type = object({
-    vpc_id              = string
-    private_subnets     = list(string)
-    public_subnets      = list(string)
+    vpc_id          = string
+    private_subnets = list(string)
+    public_subnets  = list(string)
   })
   validation {
     condition = (
-     can(regex("^vpc-\\w", var.vpc_config.vpc_id)) &&
-     alltrue([for subnet in var.vpc_config.private_subnets : can(regex("subnet-\\w+", subnet))]) &&
-     alltrue([for subnet in var.vpc_config.public_subnets : can(regex("subnet-\\w+", subnet))])
+    can(regex("^vpc-\\w", var.vpc_config.vpc_id)) &&
+    alltrue([for subnet in var.vpc_config.private_subnets : can(regex("subnet-\\w+", subnet))]) &&
+    alltrue([for subnet in var.vpc_config.public_subnets : can(regex("subnet-\\w+", subnet))])
     )
     error_message = "Invalid VPC configuration"
   }
